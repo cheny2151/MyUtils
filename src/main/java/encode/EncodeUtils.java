8ee8aa9c252b815adef4a1ca2cf70b6077b4d7e8
2151 <478241123@qq.com>
2018-01-12 17:14:28 +0800

    update

--- src/main/java/encode/EncodeUtils.java
+++ src/main/java/encode/EncodeUtils.java
@@ -8,11 +8,14 @@ public class EncodeUtils {
 
     public static String encodeDownLoadFileName(String fileName, HttpServletRequest request) throws UnsupportedEncodingException {
         if (request.getHeader("User-Agent").toLowerCase().indexOf("firefox") > 0) {
-            fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1"); // firefox浏览器
+            // firefox浏览器
+            fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
         } else if (request.getHeader("User-Agent").toUpperCase().indexOf("MSIE") > 0) {
-            fileName = URLEncoder.encode(fileName, "UTF-8");// IE浏览器
+            // IE浏览器
+            fileName = URLEncoder.encode(fileName, "UTF-8");
         } else if (request.getHeader("User-Agent").toUpperCase().indexOf("CHROME") > 0) {
-            fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");// 谷歌
+            // 谷歌
+            fileName = new String(fileName.getBytes("UTF-8"), "ISO8859-1");
         } else {
             fileName = URLEncoder.encode(fileName, "UTF-8");
         }
