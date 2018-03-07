7eeb9ff6e974c65f26f6c071321f04e848ceb2ed
2151 <478241123@qq.com>
2018-03-07 11:49:02 +0800

    super update

--- src/main/java/algorithm/HashMap/Intersection.java
+++ src/main/java/algorithm/HashMap/Intersection.java
@@ -1,5 +1,7 @@
 package algorithm.HashMap;
 
+import org.junit.Test;
+
 import java.lang.reflect.Array;
 
 /**
@@ -8,8 +10,8 @@ import java.lang.reflect.Array;
 public class Intersection {
 
 
-    private Intersection() {
-    }
+//    private Intersection() {
+//    }
 
     /**
      * 散列节点
@@ -31,77 +33,157 @@ public class Intersection {
         }
     }
 
-    private final static int MAX_LENGTH = 1 << 30;
+    /**
+     * hash桶
+     */
+    private static class HashNodes<V> {
 
-    @SuppressWarnings("unchecked")
-    public static <T> T[] exclusive(T[] forExclusive, T[] toExclusive, Class<T> resultType) {
-        int forLength = forExclusive.length;
-        int tableSize = tableForSize(forLength);
-        T[] result = (T[]) Array.newInstance(resultType, forLength);
-        Node<T>[] tables = new Node[tableSize];
-        Node<T> node;
-        boolean isExist = false;
-        int hash, index, size = 0, count = 0;
-        for (T t : forExclusive) {
-            if ((node = tables[index = (hash = hash(t)) & (tableSize - 1)]) != null) {
-                while (true) {
-                    if (node.hash == hash && node.value.equals(t)) {
-                        isExist = true;
-                        break;
+        private final static int MAX_LENGTH = 1 << 30;
+
+        private final static float LOAD_FACTOR = 0.75f;
+
+        private int size;
+
+        private Node<V>[] tables;
+
+        private int threshold;
+
+        @SuppressWarnings("unchecked")
+        public HashNodes(int length) {
+            if (length <= 0) throw new IllegalArgumentException("illegal length" + length);
+            int cap = tableForSize(length);
+            this.tables = (Node<V>[]) new Node[cap];
+            this.threshold = (int) (cap * LOAD_FACTOR);
+        }
+
+        public void insert(V v) {
+            int hash, index, cap = tables.length;
+            Node<V>[] tables = this.tables;
+            Node<V> tar, next;
+            if ((next = tables[index = ((hash = hash(v)) & (cap - 1))]) != null) {
+                do {
+                    tar = next;
+                    if (tar.hash == hash && tar.equals(v)) {
+                        return;
                     }
-                    if (node.next == null) {
-                        break;
-                    }
-                    node = node.next;
-                }
-                if (isExist) {
-                    isExist = false;
-                    continue;
-                }
-                node.next = new Node<>(hash, t, null);
+                    next = tar.next;
+                } while (next != null);
+                tar.next = new Node<>(hash, v, null);
             } else {
-                tables[index] = new Node<>(hash, t, null);
+                tables[index] = new Node<>(hash, v, null);
             }
-            if (++size > 0.75 * tables.length) {
+            if (++size > threshold) {
                 resize();
             }
         }
-        for (T t : toExclusive) {
-            if ((node = tables[(hash = hash(t)) & (tables.length - 1)]) != null) {
-                do {
-                    if (node.hash == hash && node.value.equals(t)) {
-                        result[count++] = t;
-                    }
-                } while ((node = node.next) != null);
+
+        @SuppressWarnings({"unchecked", "Duplicates"})
+        private void resize() {
+            int oldLen = tables.length;
+            int newLen;
+            if ((newLen = oldLen << 1) > MAX_LENGTH) {
+                return;
+            }
+            Node<V>[] oldTab = tables;
+            Node<V> oldNode, nextNode;
+            Node<V>[] newTab = (Node<V>[]) new Node[newLen];
+            tables = newTab;
+            threshold = (int) (LOAD_FACTOR * newLen);
+            for (int i = 0; i < oldLen; i++) {
+                if ((nextNode = oldTab[i]) != null) {
+                    Node<V> hiNode = null, loNode = null;
+                    do {
+                        oldNode = nextNode;
+                        nextNode = nextNode.next;
+                        oldNode.next = null;
+                        if ((oldNode.hash & oldLen) == 0) {
+                            if (loNode == null) {
+                                loNode = oldNode;
+                                newTab[i] = loNode;
+                            } else {
+                                loNode.next = oldNode;
+                                loNode = oldNode;
+                            }
+                        } else {
+                            if (hiNode == null) {
+                                hiNode = oldNode;
+                                newTab[i + oldLen] = hiNode;
+                            } else {
+                                hiNode.next = oldNode;
+                                hiNode = oldNode;
+                            }
+                        }
+                    } while (nextNode != null);
+                }
             }
         }
-        if (count == 0) {
-            return null;
-        } else if (count != tableSize) {
-            T[] result_2 = (T[]) Array.newInstance(resultType, count);
-            System.arraycopy(result, 0, result_2, 0, count);
-            return result_2;
+
+        @SuppressWarnings("unchecked")
+        public V[] intersect(V[] toExclusive, Class<V> resultType) {
+            Node<V>[] tables = this.tables;
+            Node<V> target;
+            int cap = tables.length;
+            V[] result = (V[]) Array.newInstance(resultType, size);
+            int count = 0;
+            for (V v : toExclusive) {
+                if ((target = tables[(hash(v) & (cap - 1))]) != null) {
+                    do {
+                        if (target.hash == hash(v) && target.value.equals(v)) {
+                            result[count++] = v;
+                        }
+                    } while ((target = target.next) != null);
+                }
+            }
+            if (count != size) {
+                V[] result_2 = (V[]) Array.newInstance(resultType, count);
+                System.arraycopy(result, 0, result_2, 0, count);
+                return result_2;
+            }
+            return result;
         }
-        return result;
+
+        private static int tableForSize(int length) {
+            length = length - 1;
+            length |= length >>> 1;
+            length |= length >>> 2;
+            length |= length >>> 4;
+            length |= length >>> 8;
+            length |= length >>> 16;
+            return length < 0 ? 0 : (length > MAX_LENGTH ? MAX_LENGTH : length + 1);
+        }
+
+        private static int hash(Object val) {
+            int hash;
+            return (val == null) ? 0 : (hash = val.hashCode()) ^ (hash >>> 16);
+        }
+
     }
 
-    private static void resize() {
-        
+
+    @SuppressWarnings("unchecked")
+    public static <T> T[] exclusive(T[] forExclusive, T[] toExclusive, Class<T> resultType) {
+        HashNodes<T> nodes = new HashNodes<>(forExclusive.length);
+        for (T t : forExclusive) {
+            nodes.insert(t);
+        }
+        return nodes.intersect(toExclusive, resultType);
     }
 
-    private static int tableForSize(int length) {
-        length = length - 1;
-        length |= length >>> 1;
-        length |= length >>> 2;
-        length |= length >>> 4;
-        length |= length >>> 8;
-        length |= length >>> 16;
-        return length < 0 ? 0 : (length > MAX_LENGTH ? MAX_LENGTH : length + 1);
+    @Test
+    public void test() {
+        Integer[] int1 = new Integer[100000];
+        Integer[] int2 = new Integer[50000];
+        for (int i = 1; i < 100001; i++) {
+            int1[i - 1] = i;
+            if ((i & 1) == 0) {
+                int2[50000 - (i >> 1)] = i;
+            }
+        }
+        long time = System.currentTimeMillis();
+        Integer[] exclusive = exclusive(int2, int1, Integer.class);
+        System.out.println("用时" + (System.currentTimeMillis() - time));
+        System.out.println("交集个数" + exclusive.length);
     }
 
-    private static int hash(Object val) {
-        int hash;
-        return (val == null) ? 0 : (hash = val.hashCode()) ^ (hash >>> 16);
-    }
 
 }
