1722819db66293fe2ca2eaa57ed8cde46dc54d87
cheney <478241123@qq.com>
2019-05-31 16:07:05 +0800

    【update】kafka

--- src/main/java/mq/kafka/producer/Producer.java
+++ src/main/java/mq/kafka/producer/Producer.java
@@ -17,7 +17,7 @@ import java.util.Properties;
  * 如果想同时对一个topic做消费的话，启动多个consumer group去消费该topic即可。
  *
  * kafka为了保证吞吐量，只允许同一个consumer group下的一个consumer线程去访问一个partition。如果觉得效率不高的时候，可以加partition的数量来横向扩展，那么再加新的consumer thread去消费。
- * 如果想多个不同的业务都需要这个topic的数据，起多个consumer group就好了，大家都是顺序的读取message，offsite的值互不影响。这样没有锁竞争，充分发挥了横向的扩展性，吞吐量极高。这也就形成了分布式消费的概念。
+ * 如果想多个不同的业务都需要这个topic的数据，起多个consumer group就好了，大家都是顺序的读取message，offset的值互不影响。这样没有锁竞争，充分发挥了横向的扩展性，吞吐量极高。这也就形成了分布式消费的概念。
  * @author cheney
  * @date 2019/5/29
  */
