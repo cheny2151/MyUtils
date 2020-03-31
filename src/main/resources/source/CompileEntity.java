67d8b38822cb878bca54c541b4aa62261e41e23e
cheney <478241123@qq.com>
2020-03-31 17:24:31 +0800

    update

---
+++ src/main/resources/source/CompileEntity.java
@@ -0,0 +1,18 @@
+package javaagent.compiler;
+
+/**
+ * @author cheney
+ * @date 2020-03-29
+ */
+public class CompileEntity {
+
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
