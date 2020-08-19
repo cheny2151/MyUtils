d106a3c8047519261f117663f2f782ad05243f8a
cheney <478241123@qq.com>
2020-08-19 20:38:31 +0800

    update redis 批量删除lua脚本

---
+++ src/main/java/lua/DeleteKey.lua
@@ -0,0 +1,21 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Cheney
+-- Date: 2020-08-19
+-- Time: 16:32
+-- To change this template use File | Settings | File Templates.
+--
+
+local len = 1
+local cursor = 0
+
+while (true) do
+    local sr = redis.call('scan', cursor, 'match', ARGV[1])
+    cursor = tonumber(sr[1])
+    local keys = sr[2]
+    for i = 1, #keys do
+        redis.call('del', keys[i])
+    end
+    len = #keys
+    if (cursor == 0 or len == 0) then break end
+end
