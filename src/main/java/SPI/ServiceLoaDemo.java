74702a0e2b73f9fe5566be3893a4d3c702641704
cheney <478241123@qq.com>
2020-08-19 13:34:37 +0800

    update SPI DEMO

---
+++ src/main/java/SPI/ServiceLoaDemo.java
@@ -0,0 +1,25 @@
+package SPI;
+
+import reflect.methodHolder.factory.MethodHolderFactory;
+
+import java.util.ServiceLoader;
+
+/**
+ * SPI使用实例:
+ * {@link ServiceLoader#load(java.lang.Class)}
+ * 会到META-INF/services的配置文件中寻找这个接口对应的实现类全路径名，
+ * 然后使用Class.forName()（传入设定的类加载器）完成类的加载。
+ *
+ * @author cheney
+ * @date 2020-08-19
+ */
+public class ServiceLoaDemo {
+
+    public static void main(String[] args) {
+        ServiceLoader<MethodHolderFactory> services = ServiceLoader.load(MethodHolderFactory.class);
+        for (MethodHolderFactory service : services) {
+            System.out.println(service.getClass());
+        }
+    }
+
+}
