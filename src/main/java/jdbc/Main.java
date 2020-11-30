472aaa4215d05724a464eac38633617ea82831f4
cheny <478241123@qq.com>
2020-11-30 09:01:32 +0800

    update

--- src/main/java/jdbc/Main.java
+++ src/main/java/jdbc/Main.java
@@ -8,17 +8,17 @@ import java.sql.*;
  */
 public class Main {
 
-    public static final String URL = "jdbc:mysql://localhost:3306/lexue?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=GMT%2B8";
+    public static final String URL = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=GMT%2B8";
     public static final String USER = "root";
     public static final String PASSWORD = "cheney@mysql";
 
     public static void main(String[] args) throws Exception {
-        //1.加载驱动程序
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        //1.jdbc4.0之后不需要手动加载，通过JDK SPI
+//        Class.forName("com.mysql.cj.jdbc.Driver");
         //2. 获得数据库连接
         Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
         Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from t_hl_grade_cost");
+        ResultSet rs = stmt.executeQuery("select * from test");
         ResultSetMetaData metaData = rs.getMetaData();
         int columnIndex = 1;
         System.out.println(metaData.getColumnClassName(columnIndex));
